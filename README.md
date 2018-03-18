5a80c711723f3af925685e288026c6ce947dd853
brilhana <alexandre.brilhante@umontreal.ca>
2018-03-18 11:42:14 -0400

    Public endpoints.

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Bitfinex
 
-[![Build Status](https://travis-ci.org/brilhana/Bitfinex.jl.svg?branch=master)](https://travis-ci.org/brilhana/Bitfinex.jl)
-
-[![Coverage Status](https://coveralls.io/repos/brilhana/Bitfinex.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/brilhana/Bitfinex.jl?branch=master)
-
-[![codecov.io](http://codecov.io/github/brilhana/Bitfinex.jl/coverage.svg?branch=master)](http://codecov.io/github/brilhana/Bitfinex.jl?branch=master)
+Julia wrapper for the Bitfinex REST API.
--- src/Bitfinex.jl
+++ src/Bitfinex.jl
@@ -1,5 +1,48 @@
 module Bitfinex
 
-# package code goes here
+using Requests
 
-end # module
+# First quick implementation of public endpoints.
+
+const SymStr = Union{Symbol, String}
+const URL = "https://api.bitfinex.com/v2/"
+
+function get_platform_status()
+    return readstring(get("$URL/platform/status"))
+end
+
+function get_tickers(symbol::SymStr)
+    if contains(symbol, " ")
+        symbol = replace(symbol, " ", "")
+    end
+    return readstring(get("$URL/tickers?symbols=$symbol"))
+end
+
+function get_ticker(symbol::SymStr)
+    return readstring(get("$URL/ticker/$symbol"))
+end
+
+function get_trades(symbol::SymStr)
+    return readstring(get("$URL/trades/$symbol/hist"))
+end
+
+function get_books(symbol::SymStr,
+                   precision::SymStr)
+    return readstring(get("$URL/book/$symbol/$precision"))
+end
+
+function get_stats(key::SymStr,
+                   size::SymStr,
+                   symbol::SymStr,
+                   side::SymStr,
+                   section::SymStr)
+    return readstring(get("$URL/stats1/:$key:$size:$symbol:$side/$section"))
+end
+
+function get_candles(timeframe::SymStr,
+                     symbol::SymStr,
+                     section::SymStr)
+    return readstring(get("$URL/candles/trade:$timeframe:$symbol/$section"))
+end
+
+end
